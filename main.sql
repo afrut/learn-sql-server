70a91e57e7e3c2001eba72b70dfc652ec07aa1b1
eric373 <25942990+eric373@users.noreply.github.com>
2022-01-12 23:31:24 -0500

    Created Northwind and pubs sample databases from Microsoft

--- main.sql
+++ main.sql
@@ -1,4 +1,8 @@
 -- Enable SQLCMD Mode. On SSMS, Query > SQLCMD Mode.
 -- Run other .sql scripts.
-:r D:\src\learn-sql-server\quickstart.sql
+-- :r D:\src\learn-sql-server\quickstart.sql
 --:r D:\src\learn-sql-server\misc.sql
+
+-- Create sample databases.
+:r D:\src\learn-sql-server\instnwnd.sql
+:r D:\src\learn-sql-server\instpubs.sql
