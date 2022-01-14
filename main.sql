1eec5a0245b50a626c60ccaad7ef1e868addc3ce
eric373 <25942990+eric373@users.noreply.github.com>
2022-01-13 19:58:26 -0500

    Replicate AdventureWorks and AdventureWorksDW for convenience

--- main.sql
+++ main.sql
@@ -1,8 +1,14 @@
 -- Enable SQLCMD Mode. On SSMS, Query > SQLCMD Mode.
 -- Run other .sql scripts.
 -- :r D:\src\learn-sql-server\quickstart.sql
---:r D:\src\learn-sql-server\misc.sql
+-- :r D:\src\learn-sql-server\misc.sql
 
 -- Create sample databases.
-:r D:\src\learn-sql-server\instnwnd.sql
-:r D:\src\learn-sql-server\instpubs.sql
+drop database if exists Northwind;
+drop database if exists pubs;
+drop database if exists AdventureWorks;
+drop database if exists AdventureWorksDW;
+:r D:\src\learn-sql-server\sample-databases\Northwind\instnwnd.sql
+:r D:\src\learn-sql-server\sample-databases\Pubs\instpubs.sql
+:r D:\src\learn-sql-server\sample-databases\AdventureWorks-oltp\instawdb.sql
+:r D:\src\learn-sql-server\sample-databases\AdventureWorksDW-olap\instawdbdw.sql
