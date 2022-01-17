9dff77a0c261664a93ffddf032573f8313de1e43
eric373 <25942990+eric373@users.noreply.github.com>
2022-01-16 21:33:56 -0500

    Replicate BikeStores database

--- main.sql
+++ main.sql
@@ -8,7 +8,9 @@ drop database if exists Northwind;
 drop database if exists pubs;
 drop database if exists AdventureWorks;
 drop database if exists AdventureWorksDW;
+drop database if exists BikeStores;
 :r D:\src\learn-sql-server\sample-databases\Northwind\instnwnd.sql
 :r D:\src\learn-sql-server\sample-databases\Pubs\instpubs.sql
 :r D:\src\learn-sql-server\sample-databases\AdventureWorks-oltp\instawdb.sql
 :r D:\src\learn-sql-server\sample-databases\AdventureWorksDW-olap\instawdbdw.sql
+:r D:\src\learn-sql-server\sample-databases\BikeStores\instbikestores.sql
