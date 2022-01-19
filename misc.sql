e412e2a63a0e1399baf1d4b7b579317a690ddd31
eric373 <25942990+eric373@users.noreply.github.com>
2022-01-18 19:59:28 -0500

    Add useful system tables

--- misc.sql
+++ misc.sql
@@ -1,6 +1,26 @@
+-- Switch context to use this database.
+use AdventureWorks;
+
 -- Check databases created.
 select *
 from sys.databases;
 
+-- See all tables.
+select *
+from sys.tables;
+
+-- See all database objects.
 select *
 from sys.objects;
+
+-- See all schemas.
+select *
+from sys.schemas;
+
+-- See all extended properties.
+select *
+from sys.extended_properties;
+
+-- See all columns.
+select *
+from sys.columns;
