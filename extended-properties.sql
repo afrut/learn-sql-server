4a8135cfdc809df44b0e29e61507a44404176390
eric373 <25942990+eric373@users.noreply.github.com>
2022-01-18 20:09:32 -0500

    Add query for inspecting extended properties of tables

--- extended-properties.sql
+++ extended-properties.sql
@@ -1,14 +1,28 @@
 use AdventureWorks;
 
 -- See the extended properties of the columns of the Person table.
-select obj.name
-	 , cols.name
-	 , ep.value
+select obj.name TableName
+	 , cols.name ColumnName
+     , ep.name PropertyName
+	 , ep.value PropertyValue
 from sys.extended_properties ep
 join sys.objects obj
 on ep.major_id = obj.object_id
 join sys.columns cols
 on obj.object_id = cols.object_id
 and ep.minor_id = cols.column_id
-where obj.name = 'Person'
-  and ep.class_desc = 'OBJECT_OR_COLUMN';
+where ep.class_desc = 'OBJECT_OR_COLUMN'
+  and obj.name = 'Person'
+order by obj.name asc
+	   , cols.name asc;
+
+-- See all extended properties of all user tables.
+select tbl.name TableName
+	 , ep.name PropertyName
+	 , ep.value PropertyValue
+from sys.tables tbl
+join sys.extended_properties ep
+on tbl.object_id = ep.major_id
+where tbl.type_desc = 'USER_TABLE'
+  and ep.minor_id = 0
+ order by tbl.name;
